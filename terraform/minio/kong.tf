272d56b2b0a8ddf85b566c32ce1daaca770e33c4
liyangau <d@aufomm.com>
2025-10-16 11:20:43 +1100

    change policies to access minio bucket

--- terraform/minio/kong.tf
+++ terraform/minio/kong.tf
@@ -2,60 +2,81 @@ resource "minio_s3_bucket" "kong_backup" {
   bucket = "kong-backup"
 }
 
-resource "minio_iam_user" "kong_admin_user" {
-  name = "kong-backup-admin"
-}
-
-resource "minio_iam_user" "kong_readonly_user" {
-  name = "kong-backup-readonly"
-}
-
-resource "minio_iam_policy" "kong_admin_policy" {
+resource "minio_iam_policy" "kong_backup_admin" {
   name = "kong-backup-admin-policy"
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
-        Effect = "Allow",
-        Action = ["s3:*"],
-        Resource = [
-          "arn:aws:s3:::kong-backup",
-          "arn:aws:s3:::kong-backup/*"
+        "Effect": "Allow",
+        "Action": [
+          "s3:ListBucket",
+          "s3:GetBucketLocation"
+        ],
+        "Resource": "arn:aws:s3:::${minio_s3_bucket.kong_backup.bucket}"
+      },
+      {
+        "Effect": "Allow",
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:AbortMultipartUpload",
+          "s3:ListMultipartUploadParts"
         ]
+        "Resource": "arn:aws:s3:::${minio_s3_bucket.kong_backup.bucket}/*"
       }
     ]
   })
 }
 
 # Read-only policy
-resource "minio_iam_policy" "kong_readonly_policy" {
+resource "minio_iam_policy" "kong_backup_readonly" {
   name = "kong-backup-readonly-policy"
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
-        Effect = "Allow",
-        Action = [
-          "s3:GetObject",
-          "s3:ListBucket"
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:ListBucket",
+          "s3:GetBucketLocation"
         ],
-        Resource = [
-          "arn:aws:s3:::kong-backup",
-          "arn:aws:s3:::kong-backup/*"
-        ]
+        "Resource": "arn:aws:s3:::${minio_s3_bucket.kong_backup.bucket}"
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : ["s3:GetObject"],
+        "Resource": "arn:aws:s3:::${minio_s3_bucket.kong_backup.bucket}/*"
       }
     ]
   })
 }
 
 # Attach admin policy
-resource "minio_iam_user_policy_attachment" "kong_admin_attachment" {
-  user_name   = minio_iam_user.kong_admin_user.name
-  policy_name = minio_iam_policy.kong_admin_policy.name
+resource "minio_iam_user_policy_attachment" "kong_backup_admin_attachment" {
+  user_name   = minio_iam_user.kong_backup_admin.name
+  policy_name = minio_iam_policy.kong_backup_admin.name
 }
 
 # Attach read-only policy
-resource "minio_iam_user_policy_attachment" "kong_readonly_attachment" {
-  user_name   = minio_iam_user.kong_readonly_user.name
-  policy_name = minio_iam_policy.kong_readonly_policy.name
+resource "minio_iam_user_policy_attachment" "kong_backup_readonly_attachment" {
+  user_name   = minio_iam_user.kong_backup_readonly.name
+  policy_name = minio_iam_policy.kong_backup_readonly.name
+}
+
+resource "minio_iam_user" "kong_backup_admin" {
+  name = "kong-backup-admin"
+}
+
+resource "minio_iam_user" "kong_backup_readonly" {
+  name = "kong-backup-readonly"
+}
+
+resource "minio_iam_service_account" "kong_backup_admin" {
+  target_user = minio_iam_user.kong_backup_admin.name
+}
+
+resource "minio_iam_service_account" "kong_backup_readonly" {
+  target_user = minio_iam_user.kong_backup_readonly.name
 }
